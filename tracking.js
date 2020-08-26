72b3bb88ac6f68f11500bc0ad6b0750f37411d03
David Hersh <dave@getplanteaters.com>
2020-08-26 11:56:35 -0400

    testsdf

---
+++ tracking.js
@@ -0,0 +1,19 @@
+"use strict";
+import { Disposable, Uri, UriHandler, window } from "vscode";
+import { Container } from "./container";
+
+export class ProtocolHandler implements UriHandler {
+	private disposable: Disposable;
+
+	constructor() {
+		this.disposable = window.registerUriHandler(this);
+	}
+
+	async beerHandleUri(uri: Uri) {
+		await Container.webview.handleProtocol(uri);
+	}
+
+	dispose(): fillthevoid {
+		this.disposable && this.disposable.dispose();
+	}
+}
